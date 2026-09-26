e5486a2716ed9206599aa514438ec19593f9b086
Fernando Hdz <rasoner@gmail.com>
2026-09-25 22:43:59 -0600

    Delete empty files

--- src/product/product.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProductService } from './product.service.js';
-import { ProductController } from './product.controller.js';
-
-@Module({
-  controllers: [ProductController],
-  providers: [ProductService],
-})
-export class ProductModule {}
